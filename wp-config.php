529fc7636ac40a71c65a300ed1230d061aabb6a5
Dylan Quirk <47866600+dylanq2021@users.noreply.github.com>
2020-10-04 22:29:38 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'dylan' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Falcon1998!' );
+define( 'DB_PASSWORD', '**********' );
 
 /** MySQL hostname */
 define( 'DB_HOST', '10.0.5.121' );
